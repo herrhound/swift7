48cfc16239d09e58ad34b11b0387f2af76410230
Nikola Tonkev <nikola_tonkev@yahoo.com>
2014-06-07 22:36:06 -0400

    Initial Commit

---
+++ Swift7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift7
+//
+//  Created by Nikola Tonkev on 2014-06-07.
+//  Copyright (c) 2014 nousdynamics.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
